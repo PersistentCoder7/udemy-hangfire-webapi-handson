9a70bb9ad6bb183a11bee4f7c06333b9fe8c940f
Prabhugurudarshan Singh <prabhu25.1975@gmail.com>
2023-10-10 21:34:37 +0530

    Implemented the fire and forget type of job.

--- hangfire-webapi/Controllers/HangfireController.cs
+++ hangfire-webapi/Controllers/HangfireController.cs
@@ -4,11 +4,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hangfire;
 
 namespace hangfire_webapi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class HangfireController : ControllerBase
     {
         [HttpGet]
@@ -16,6 +17,18 @@ namespace hangfire_webapi.Controllers
         {
             return Ok("Hello from hangfire api!");
         }
-        
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Welcome()
+        {
+            var JobId = BackgroundJob.Enqueue(() => SendWelcomeEmail("Welcome to our app"));
+            return Ok($"Job Id: {JobId}, Welcome email sent to the user!");
+        }
+
+        public void SendWelcomeEmail(string msg)
+        {
+            Console.WriteLine(msg);
+        }
     }
 }
